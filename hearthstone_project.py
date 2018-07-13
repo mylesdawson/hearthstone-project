6bbde72c2a2228895d3101fe1b48aa1a1032e20e
mylesdawson <mylesdawson@gmail.com>
2018-07-12 21:04:06 -0700

    Further cleaning of data

--- hearthstone_project.py
+++ hearthstone_project.py
@@ -1,24 +1,53 @@
 import sys
 import numpy as np
 import pandas as pd
+import re
 
 
 def clean_data(df):
     unused_columns = ['artist', 'cardId', 'cardSet', 'collectible', 'dbfId', 'locale', 'flavor', 'howToGet', 'howToGetGold', 'img', 'imgGold', 'multiClassGroup', 'elite', 'classes', 'faction']
     df = df.drop(unused_columns, axis=1)
-    df = df[df['type'] != 'Hero']
+    df = df[df['type'] == 'Minion']
+    df['text'] = df['text'].astype(str)
     return df
 
-def main():
-    # Please use the provided cards.json file as the argument
-    filename = sys.argv[1]
+def transform_mechanics(x):
+    flat = []
+    if str(x) == 'nan':
+        return flat
+    for item in x:
+        for key in item:
+            flat.append(item[key])
+    return flat
 
+def filter_text(x):
+    # Removes html tags </> from card text and square brackets []
+    remove_x = re.compile(r'\[x\]')
+    remove_brackets = re.compile(r'<.*?>')
+    x = remove_x.sub('', x)
+    x = remove_brackets.sub('', x)
+    x = x.strip()
+    x = x.replace('\\n', ' ')
+    return x
+
+def main(filename):
+    # Please use the provided cards.json file as the argument
     data = pd.read_json(filename, lines=True)
 
-    # We now have 3 types of cards: Weapon, Minion, Spell
+    # We now only have one type of card: Minion
     data = clean_data(data)
+    # Transform mechanics to 1-d array of strings
+    data['mechanics'] = data['mechanics'].apply(transform_mechanics)
+    # Filter out unneeded text 
+    data['text'] = data['text'].apply(filter_text)
+
+    # We may not need mechanics (remove?)
+    # Try creating normal distribution and sampling from it to create new cards
+    # Try using simple reccurence neural network to generate cards
+
+
     print(data)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
