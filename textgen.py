8f51402ff47e695edb02bdd8d58743abf83e2dce
mylesdawson <mylesdawson@gmail.com>
2018-07-23 20:03:50 -0700

    Updated card generator. Added sample generated cards

--- textgen.py
+++ textgen.py
@@ -2,12 +2,12 @@ import sys
 from textgenrnn import textgenrnn
 
 
-def main(input_text):
+def main(input_text, generated_output):
 
     textgen = textgenrnn()
-    textgen.train_from_file(input_text, num_epochs=5)
-    textgen.generate()
+    textgen.train_from_file(input_text, num_epochs=2)
+    textgen.generate_to_file(generated_output, temperature=0.5, n=20)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
