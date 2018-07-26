106275b1f8b38869dd0ef05989c1f8dbe24931e8
mylesdawson <mylesdawson@gmail.com>
2018-07-26 16:42:02 -0700

    Modified clean_cards to include spells. Now writes data to 3 files (text,name,rest)

--- clean_cards.py
+++ clean_cards.py
@@ -5,46 +5,29 @@ import re
 
 
 def clean_data(df):
-    unused_columns = ['artist', 'cardId', 'cardSet', 'collectible', 'dbfId', 'locale', 'flavor', 'howToGet', 'howToGetGold', 'img', 'imgGold', 'multiClassGroup', 'elite', 'classes', 'faction', 'armor', 'durability']
+    unused_columns = ['artist', 'cardId', 'cardSet', 'collectible', 'dbfId', 'locale', 'flavor', 'howToGet', 'howToGetGold', 'img', 'imgGold', 'multiClassGroup', 'elite', 'classes', 'faction', 'armor', 'durability', 'mechanics']
     df = df.drop(unused_columns, axis=1)
-    df = df[df['type'] == 'Minion']
     df['text'] = df['text'].astype(str)
     return df
 
-def transform_mechanics(x):
-    flat = []
-    if str(x) == 'nan':
-        return flat
-    for item in x:
-        for key in item:
-            flat.append(item[key])
-    return flat
-
 def filter_text(x):
-    # Removes html tags </> from card text and square brackets []
-    remove_x = re.compile(r'\[x\]')
     remove_tags = re.compile(r'<.*?>')
-    x = remove_x.sub('', x)
     x = remove_tags.sub('', x)
     x = x.strip()
     x = x.replace('\\n', ' ')
     return x
 
-def main(filename, output_file):
+def main(filename, card_text_file, card_name_file, card_rest_file):
     # Please use the provided cards.json file as the argument
     data = pd.read_json(filename, lines=True)
 
-    # We now only have one type of card: Minion
     data = clean_data(data)
-    # Transform mechanics to 1-d array of strings
-    data['mechanics'] = data['mechanics'].apply(transform_mechanics)
-    # Filter out unneeded text 
     data['text'] = data['text'].apply(filter_text)
 
-    data['text'] = data['text'].replace('nan', np.NaN)
-    data = data.dropna()
+    data['text'].to_csv(card_text_file, header=None, index=None, sep=' ', mode='a')
+    data['name'].to_csv(card_name_file, header=None, index=None, sep=' ', mode='a')
+    data[['attack', 'cost', 'health', 'playerClass', 'race', 'rarity']].to_csv(card_rest_file, header=None, index=None, sep=' ', mode='a')
 
-    data.to_json(output_file, orient="records", lines=True)
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
