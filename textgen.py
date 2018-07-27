c200d90ddeb99940700d632dabe5e502c587c8fb
mylesdawson <mylesdawson@gmail.com>
2018-07-26 20:44:46 -0700

    updated texgen with less epochs

--- textgen.py
+++ textgen.py
@@ -5,7 +5,7 @@ from textgenrnn import textgenrnn
 def main(input_text):
 
     textgen = textgenrnn(name="new_model")
-    textgen.train_from_file(input_text, num_epochs=10, rnn_bidirectional=True)
+    textgen.train_from_file(input_text, num_epochs=3, rnn_bidirectional=True)
     textgen.generate_to_file('generated_output-0.5.txt', temperature=0.2, n=20)
     textgen.generate_to_file('generated_output-0.2.txt', temperature=0.5, n=20)
     textgen.generate_to_file('generated_output-1.txt', temperature=1, n=20)
