fe6cb582a66dca34b044c0fd7d49756b318f5699
mylesdawson <mylesdawson@gmail.com>
2018-07-26 19:06:32 -0700

    Updated texgen to output more realistic cards

--- textgen.py
+++ textgen.py
@@ -2,12 +2,15 @@ import sys
 from textgenrnn import textgenrnn
 
 
-def main(input_text, generated_output):
+def main(input_text):
 
-    textgen = textgenrnn()
-    textgen.train_from_file(input_text, num_epochs=2)
-    textgen.generate_to_file(generated_output, temperature=0.5, n=20)
+    textgen = textgenrnn(name="new_model")
+    textgen.train_from_file(input_text, num_epochs=10, rnn_bidirectional=True)
+    textgen.generate_to_file('generated_output-0.5.txt', temperature=0.2, n=20)
+    textgen.generate_to_file('generated_output-0.2.txt', temperature=0.5, n=20)
+    textgen.generate_to_file('generated_output-1.txt', temperature=1, n=20)
+
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1])
